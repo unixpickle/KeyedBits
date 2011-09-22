50c16f6a3d0840868744eeee824cf3698b6f8ddd
Alex Nichol <alexnichol@comcast.net>
2011-09-22 17:45:12 -0400

    Initial Commit

---
+++ KeyedBits/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KeyedBits
+//
+//  Created by Alex Nichol on 9/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
